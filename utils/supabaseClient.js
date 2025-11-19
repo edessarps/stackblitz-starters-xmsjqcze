908aeae4d55b59c293d159c516bd82f26f798ecb
edessarps <edessarps@gmail.com>
2025-11-19 22:12:29 +0100

    test

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+// REMPLACEZ CI-DESSOUS PAR VOTRE VRAIE URL (celle qui commence par https://)
+const supabaseUrl = 'https://bphwvybuhytsxevvugxhu.supabase.co' 
+
+// REMPLACEZ CI-DESSOUS PAR VOTRE VRAIE CLÉ (celle qui commence par eyJh...)
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwaHd2eWJ1aHl0c3hldnZ1Z3hodSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzM3MTI2Mzc5LCJleHAiOjIwNTI3MDIzNzl9.wIIMKiWcc42r2AOSnqoA1pFPpmoqal-hauE'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
