59fae4ebbc0f480c3bebf30bbad022153520eab9
edessarps <edessarps@gmail.com>
2025-11-19 22:15:07 +0100

    test2

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 // REMPLACEZ CI-DESSOUS PAR VOTRE VRAIE URL (celle qui commence par https://)
-const supabaseUrl = 'https://bphwvybuhytsxevvugxhu.supabase.co' 
+const supabaseUrl = 'https://bphwybuhytsxevvugxhu.supabase.co' 
 
 // REMPLACEZ CI-DESSOUS PAR VOTRE VRAIE CLÉ (celle qui commence par eyJh...)
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwaHd2eWJ1aHl0c3hldnZ1Z3hodSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzM3MTI2Mzc5LCJleHAiOjIwNTI3MDIzNzl9.wIIMKiWcc42r2AOSnqoA1pFPpmoqal-hauE'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwaHd5YnVoeXRzeGV2dnVneGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzODU4MDAsImV4cCI6MjA3ODk2MTgwMH0.y5TgsneXqMIMKiWcc42r2A0SnqoA1pFPpmoqal-hauE'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
